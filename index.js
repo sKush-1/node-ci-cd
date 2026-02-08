f0de784e3f83bb4e80eca23077b72f84cebf7a31
sKush-1 <kushsumit27@gmail.com>
2026-02-08 20:59:44 +0530

    setup nodeserver with docker

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Health check ok!",
+  });
+});
+
+app.listen(3421, () => {
+  console.log("Server is running on port 3421");
+});
